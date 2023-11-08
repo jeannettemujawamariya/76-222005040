6649f006b94b85f35dec9f60c4dc4bfcb6a725ca
jeannettemujawamariya <149788207+jeannettemujawamariya@users.noreply.github.com>
2023-11-08 19:12:47 +0200

    Add files via upload

---
+++ work2inheltance.java
@@ -0,0 +1,28 @@
+public class Visa{
+    public void Give(){
+        System.out.println("Give me the result");
+    }
+}
+public class Nationality extends Visa{
+    public void get(){
+        System.out.println(" You can fly");
+    }
+}
+interface E{
+     void print();
+}
+class Me extends Age implements E{
+    void print(){
+        System.out.println(" she is  tuenty five years old"); 
+    
+  }
+}
+  class Question1{
+ public static void main(String[] args){
+    Visa d=new Visa();
+    d.Give();
+    d.get();
+    d.print();
+    d.Hi();
+ }
+  }
